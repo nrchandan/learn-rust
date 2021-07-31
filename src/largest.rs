b8a8b6fc96cd37dae2ba20ace86e00fa7d764a9b
Chandan Kumar <chandan.gupta@gmail.com>
2021-07-31 13:39:47 +0530

    Use alternate implementation for basic data types using Copy trait

--- src/largest.rs
+++ src/largest.rs
@@ -1,8 +1,8 @@
-fn largest<T: PartialOrd>(list: &[T]) -> &T {
-    let mut largest = &list[0];
-    for item in list {
+fn largest<T: PartialOrd + Copy>(list: &[T]) -> T {
+    let mut largest = list[0];
+    for &item in list {
         if item > largest {
-            largest = &item;
+            largest = item;
         }
     }
     largest
